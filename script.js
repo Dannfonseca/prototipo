35790fa4f99f8b7585a749411c8684a71484f3bc
Daniel Nascimento <114073975+Dannfonseca@users.noreply.github.com>
2023-10-09 12:55:18 -0300

    Update script.js
    Commit parcial final js

--- script.js
+++ script.js
@@ -1,14 +1,82 @@
 let mercadorias = {
-    Margarina: 0,
-    Melão: 0,
-    Alface: 0,
-    Manteiga: 0,
-    Hamburguer: 0,
+    Amendoim: 0,
+    Aveia: 0,
+    Paçoca: 0,
+    Leite: 0,
+    Ovomaltine: 0,
+    Chocoball: 0,
+    Sucrilhos: 0,
+    Granola: 0,
+    Jujuba: 0,
+    "FlocosArroz": 0,
+    "GranuladoPreto": 0,
+    "GranuladoColorido": 0,
+    "BiscoitinhoAçaí": 0,
+    '150ml': 0,
+    '200ml': 0,
+    '250ml': 0,
+    '300ml': 0,
+    '400ml': 0,
+    '500ml': 0,
+    '770ml': 0,
+    Coco: 0,
+    Flocos: 0,
+    Napolitano: 0,
+    MiniSaia: 0,
+    Milhoverde: 0,
+    Limão: 0,
+    BalaMole: 0,
+    CartelaChiclete: 0,
+    Mentos: 0,
+    Freegels: 0,
+    HallsCereja: 0,
+    Trento: 0,
+    Paçoquita: 0,
+    ChicleteBlong: 0,
+    Trakinas: 0,
+    Zumbitos: 0,
+    DoceAmendoim: 0,
+    Pirulito: 0,
+    DoceAbóbora: 0,
+    PéDeMoça: 0,Chocolate: 0,
+    Morango: 0,
+    Uva: 0,
+    'Tuti-frutti': 0,
+    Caramelo: 0,
+    LeiteCondensado: 0,
+    Menta: 0,
+    MaçãVerde: 0,
+    Graviola: 0,
     Açaí: 0,
-    Tomate: 0,
-    Cebola: 0,
-    Copo200ml: 0,
-    Copo300ml: 0
+    SorveteFlocos: 0,
+    SorveteNapolitano: 0,
+    SorveteCreme: 0,
+    'Coca-cola2lt': 0,
+    'Coca lata': 0,
+    'Artártica2lt': 0,
+    'Antártica1lt': 0,
+    'Antárticalata': 0,
+    'Tobi2lt': 0,
+    'Guaravita': 0,
+    'ConvençãoPet': 0,
+    'ÁguaMineral': 0,
+    'Hamburguer': 0,
+    'HamburguerPicanha': 0,
+    'Bacon': 0,
+    'BatataPalha': 0,
+    'BatataFrita': 0,
+    'Mussarela': 0,
+    'Apresuntado': 0,
+    'LinguiçaFatiada': 0,
+    'Arroz': 0,
+    'Feijão': 0,
+    'Açúcar': 0,
+    'LeiteIntegral': 0,
+    'LeitePó': 0,
+    'FiléPeitoFrango': 0,
+    'Carne': 0,
+    'CartelaOvos': 0
+    
 };
 
 function incrementar(mercadoria) {
@@ -25,15 +93,12 @@ function decrementar(mercadoria) {
 
 function enviarWhatsApp() {
     let mensagem = gerarMensagem();
-    let numeroWhatsApp = "5521989324855"; // Número de WhatsApp desejado
-
-    // Codificar a mensagem para ser usada no URL
+    let numeroWhatsApp = "21989324855"; 
+    
     mensagem = encodeURIComponent(mensagem);
-
-    // Construir o link do WhatsApp com o número e a mensagem
+    
     const linkWhatsApp = `https://wa.me/${numeroWhatsApp}?text=${mensagem}`;
-
-    // Abrir o link no WhatsApp
+   
     window.open(linkWhatsApp, "_blank");
 }
 
